4cdd73ff66d5e580236f02fa4f8b1c67bb8197e2
Pranay Bhardwaj <235537439+PranayBhardwaj29@users.noreply.github.com>
2026-03-26 12:02:57 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#container");
+
+for (let index = 0; index < 256; index++) {
+  const newDiv = document.createElement("div");
+  newDiv.className = "squareGrid";
+  newDiv.addEventListener("mouseover", function () {
+    const randomColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    newDiv.style.backgroundColor = randomColor;
+  });
+  container.appendChild(newDiv);
+}
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  const squares = prompt("Enter the squares you want per side: ");
+  if (squares > 100 || squares < 1) {
+    alert("Invalid input");
+    return;
+  } else {
+    container.innerHTML = "";
+    console.log("Cleared");
+    for (let index = 0; index < squares * squares; index++) {
+      const newDiv = document.createElement("div");
+      newDiv.className = "squareGrid";
+      newDiv.addEventListener("mouseover", function () {
+        const randomColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+        newDiv.style.backgroundColor = randomColor;
+      });
+      newDiv.style.width = 400 / squares + "px";
+      newDiv.style.height = 400 / squares + "px";
+      container.appendChild(newDiv);
+    }
+  }
+});
